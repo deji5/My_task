18ee21d9d81c256bea21c5fa871e7fb0e2d62d58
oni adedeji <onideji6@gmail.com>
2025-08-12 07:11:07 -0700

    adeed new files

--- week_2/task.py
+++ week_2/task.py
@@ -1,3 +1,14 @@
 # Task 1
 quote =input("please input your favourite quote: ")
-quotes = quote
+quotes = quote.title()
+print(f"\"{quotes}\"")
+
+# Task 2
+shopping_list = []
+for i in range (3) :
+    item = input(f"Enter shopping item {i+1}: ")
+    shopping_list.append(item)
+    print(", ".join(shopping_list))
+
+# Task 3
+print("i am a boy")
